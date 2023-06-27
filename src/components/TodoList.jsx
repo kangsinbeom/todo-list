e98bc5e122e12547715c3629ae39d0ff74461938
kangsinbeom <83047601+kangsinbeom@users.noreply.github.com>
2023-06-27 14:14:00 +0900

    contextAPI 적용중 근데 redux로 변경 예정

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,44 +1,21 @@
-import React, { useState } from 'react';
 import TodoListItems from './TodoListItems';
-import CompleteTodo from './CompleteTodo';
 import styled from "styled-components"
 
 function TodoList({todoList, setTodoList}) {
-    const [completeList, setCompleteList] = useState([]);
     
-    const removeTodo = (id) => {
-        const newList = todoList.filter(todo => todo.id !== id);
-        setTodoList(newList)
-    }
-
-    const todoToComplete = (id) => {
-        const newComplete = todoList.find(todo => todo.id === id);
-        setCompleteList([...completeList, newComplete])
-
-        const newList = todoList.filter(todo => todo.id !== id);
-        setTodoList(newList)
-    }
-
-    const CompleteToTodo = (id) => {
-        const newTodo = completeList.find(todo => todo.id === id);
-        setTodoList([...todoList, newTodo])
-
-        const newList = completeList.filter(todo => todo.id !== id);
-        setCompleteList(newList)
-    }
-
+    
     return (
         <>
             <h2>working</h2>
             <StyledList>
                 {
-                todoList.map(todo => <TodoListItems todo ={todo} key={todo.id} removeTodo ={removeTodo} todoToComplete ={todoToComplete} />)
+                todoList.map(todo => <TodoListItems todo ={todo} key={todo.id}/>)
                 }
             </StyledList>
             <h2>Done</h2>
             <StyledList>
                 {
-                completeList.map(todo => <CompleteTodo todo ={todo} key={todo.id} removeTodo ={removeTodo} CompleteToTodo ={CompleteToTodo} />)
+                todoList.map(todo => <TodoListItems todo ={todo} key={todo.id}/>)
                 }
             </StyledList>    
         </>
